617db1580a4b2cffa3e7a45dda7c6cdf6029d8d6
IyanuKwent <olandria.iankent@gmail.com>
2025-03-07 00:23:02 +0800

    remove some comments

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function displayCourses(courses) {
     const courseList = document.getElementById("course-list");
-    courseList.innerHTML = ""; // Clear the default "Loading courses..." message
+    courseList.innerHTML = "";
 
     courses.forEach(course => {
         const courseHeader = document.createElement("h3");
@@ -41,19 +41,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const profilePic = document.getElementById("profile-pic");
 
     document.getElementById("about").addEventListener("mouseover", function () {
-        profilePic.src = "images/pfp.png"; // Change this to your About Me image
+        profilePic.src = "images/pfp.png";
     });
 
     document.getElementById("education").addEventListener("mouseover", function () {
-        profilePic.src = "images/pfp2.png"; // Change this to your Education image
+        profilePic.src = "images/pfp2.png"; 
     });
 
     document.getElementById("skills").addEventListener("mouseover", function () {
-        profilePic.src = "images/pfp3.png"; // Change this to your Skills image
+        profilePic.src = "images/pfp3.png";
     });
 
-    // Optional: Reset the image when not hovering
+ 
     document.querySelector(".right").addEventListener("mouseleave", function () {
-        profilePic.src = "images/pfp.png"; // Default image
+        profilePic.src = "images/pfp.png"; 
     });
 });
